86cffc92e26f8f638131061bb0d30598ca026782
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-12-12 21:04:25 -0300

    feat: completed validator

--- src/create.notification.body.ts
+++ src/create.notification.body.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
 export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
   recipientId: string;
+  @IsNotEmpty()
+  @Length(3, 280)
   content: string;
+  @IsNotEmpty()
   category: string;
 }
