19d0ddd8f7c0258366c476a5c20cef56c28535f4
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-12-14 19:05:34 -0300

    feat: first tests

--- src/app/entities/NotificationContent/index.spec.ts
+++ src/app/entities/NotificationContent/index.spec.ts
@@ -5,4 +5,13 @@ describe('NotificationContent', () => {
     const result = new NotificationContent('Hi, how are you');
     expect(result.value).toBe('Hi, how are you');
   });
+
+  it('should not be able to create a notification with few chars', () => {
+    expect(() => new NotificationContent('Hi')).toThrow();
+  });
+
+  it('should not be able to create a notification with lots of chars', () => {
+    const input = 'a'.repeat(300);
+    expect(() => new NotificationContent(input)).toThrow();
+  });
 });
