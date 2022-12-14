509c56bf1f6976622a84bd291a10a8a0657cb295
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-12-14 18:48:11 -0300

    feat: created notification content class

---
+++ src/app/entities/NotificationContent.ts
@@ -0,0 +1,16 @@
+export class NotificationContent {
+  constructor(private readonly content: string) {
+    if (!this.validate(content)) {
+      throw new Error('Lenght error');
+    }
+    this.content = content;
+  }
+
+  get value() {
+    return this.content;
+  }
+
+  validate(content: string): boolean {
+    return content.length >= 5 && content.length <= 240;
+  }
+}
