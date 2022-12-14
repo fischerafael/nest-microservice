0d70a5a1a2d5dbcd11216ea5eec0b9b0e87b4a52
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-12-14 18:41:01 -0300

    feat: adjusted tsconfig

---
+++ src/app/entities/Notification.ts
@@ -0,0 +1,54 @@
+export class Notification {
+  private props: INotificationProps;
+
+  constructor(props: INotificationRequest) {
+    this.props = props;
+  }
+
+  public set recipientId(data: string) {
+    this.props.recipientId = data;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(data: string) {
+    this.props.content = data;
+  }
+
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set category(data: string) {
+    this.props.category = data;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(data: Date | null | undefined) {
+    this.props.readAt = data;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+}
+
+export interface INotificationRequest {
+  content: string;
+  category: string;
+  recipientId: string;
+  createdAt: Date;
+}
+
+export interface INotificationProps extends INotificationRequest {
+  content: string;
+  category: string;
+  recipientId: string;
+  createdAt: Date;
+  readAt?: Date | null;
+}
