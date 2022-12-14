0d0c55ebc9758690367458fbc088fa73a0f458f3
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-12-14 20:21:32 -0300

    feat: implementing fake repository

--- src/app/useCases/SendNotification/send-notification.spec.ts
+++ src/app/useCases/SendNotification/send-notification.spec.ts
@@ -1,8 +1,11 @@
+import { NotificationRepositoryInMemory } from 'src/infra/repositories/Notification/NotificationRepositoryInMemory';
 import { SendNotificationUseCase } from '.';
 
 describe('SendNotificationUseCase', () => {
   it('should be able to send a notification', async () => {
-    const notification = new SendNotificationUseCase();
+    const notification = new SendNotificationUseCase(
+      new NotificationRepositoryInMemory(),
+    );
     const result = await notification.execute({
       category: 'testeteste',
       content: 'testeteste',
--- src/infra/repositories/Notification/NotificationRepositoryInMemory/index.ts
+++ src/infra/repositories/Notification/NotificationRepositoryInMemory/index.ts
@@ -2,7 +2,5 @@ import { Notification } from 'src/app/entities/Notification';
 import { NotificationRepository } from 'src/app/repositories/Notification/NotificationRepository';
 
 export class NotificationRepositoryInMemory implements NotificationRepository {
-  create(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
+  async create(notification: Notification): Promise<void> {}
 }
